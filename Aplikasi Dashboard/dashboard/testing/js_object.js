ca882a2004e10d5b4636d26d6142d32778f66ae6
Indra Setiawan <77474939+indrasetiawan99@users.noreply.github.com>
2022-07-25 04:23:51 +0700

    Delete Aplikasi Dashboard/dashboard directory

--- Aplikasi Dashboard/dashboard/testing/js_object.js
+++
@@ -1,9 +0,0 @@
-var myObj, x, lengthObj2, keyObj2;
-myObj = {"name":"John", "age":30, "car":null};
-myObj2 = {"ext1":"nothing1", "ext2":"nothing2"};
-lengthObj2 = Object.keys(myObj2).length;
-keyObj2 = Object.keys(myObj2);
-
-for(var i = 0;i < lengthObj2;i++){
-	myObj[keyObj2[i]] = myObj2[keyObj2[i]];
-}
