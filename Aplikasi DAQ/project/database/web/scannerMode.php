2a24f2bb08404a2ae290d08c8da627a2ca468ecc
Indra Setiawan <77474939+indrasetiawan99@users.noreply.github.com>
2022-07-26 04:24:23 +0700

    Delete Aplikasi DAQ directory

--- Aplikasi DAQ/project/database/web/scannerMode.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (!empty($_POST['Mode'])) {
-    include('../connection.php');
-    $mode = $_POST['Mode'];
-    $query = "UPDATE scanner_mode SET mode = '$mode'";
-    $mysqli->query($query);
-    $mysqli->close();
-}
--- Aplikasi DAQ/project/tester/generate_qrcode.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('../assets/vendor/autoload.php');
-
-$valQRcode = '30-004-111' . '_' . 'API' . '_' . '10' . '_' . '0030' . '_' . '250321' . '_' . '07.24.00';
-$qrCode = new Endroid\QrCode\QrCode($valQRcode);
-$qrCode->setSize(65);
-// $qrCode->setSize(96.75);
-$qrCode->setMargin(0);
-$qrCode->writeFile('../upload/qrcode/item-' . $valQRcode . '.png');
